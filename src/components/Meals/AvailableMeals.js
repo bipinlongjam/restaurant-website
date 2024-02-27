775406e4f4555fa451a5a5b31d7f760c0371cac8
bipinlongjam <bipinlongjam11@gmail.com>
2024-02-27 12:23:41 +0530

    add item list style

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,4 +1,6 @@
+import Cards from '../UI/Cards';
 import classes from './AvailableMeals.module.css'
+import MealItem from './MealItem/MealItem';
 
 const DUMMY_MEALS = [
     {
@@ -29,13 +31,21 @@ const DUMMY_MEALS = [
   
   const AvailableMeals = () => {
 
-    const mealsList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>)
+    const mealsList = DUMMY_MEALS.map(meal => 
+    <MealItem 
+    key={meal.id} 
+    name={meal.name} 
+    description={meal.description}
+    price={meal.price}
+    />)
 
     return (
       <section className={classes.meals}>
-        <ul>
+       <Cards>
+       <ul>
             {mealsList}
         </ul>
+       </Cards>
       </section>
     )
   }
